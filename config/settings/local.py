a064bcc98dba721a8c33bcc875e0da4595f994c8
hanz tura <hanztura@github.com>
2020-04-24 21:12:12 +0800

    Custom sekizai template tag to integrate multi sekizai with django-compressor

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,4 +12,4 @@ INTERNAL_IPS = ['127.0.0.1']
 
 INSTALLED_APPS += ['debug_toolbar', ]
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
-COMPRESS_ENABLED = False
+COMPRESS_ENABLED = True
